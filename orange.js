2094b9ac06b322ccd11157d36895e642d3d26ce6
SaniaKPI <37076967+SaniaKPI@users.noreply.github.com>
2019-12-25 21:21:02 +0200

    Delete orange.js

--- orange.js
+++
@@ -1,9 +0,0 @@
-document.getElementsByTagName("tbody")[0].addEventListener("click",func);
-var popup = document.getElementsByClassName("popup")[0];
-function func(event){
-	if(event.target.innerText == "ЗАКАЗАТЬ РЕЙС" ){
-		popup.innerText = event.target.previousElementSibling.previousElementSibling.previousElementSibling.innerText +
-		" | " + event.target.previousElementSibling.previousElementSibling.innerText;
-
-	}
-}
