2cc9ccd2f4ae3c28bd4f683d293edf64da24d6fa
SaniaKPI <37076967+SaniaKPI@users.noreply.github.com>
2020-01-06 14:56:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,25 +9,21 @@ buttonAddToArray.addEventListener("click",addToArray);
 buttonRemoveLast.addEventListener("click",removeLastElemOfArray);
 buttonClearArray.addEventListener("click",clearArray);
 
-let array = [];
+let array= [];
 
- function createLargestNumber() {						
+function createLargestNumber() {
     if (!array.length){
-        alert("Array is empty!");
+        alert("Array is empty!!");
         return;
     }
-    let sortedArray = bubbleSort(array);
-
-    alert("result is " + Number(sortedArray.join('')));
+    alert("result is " + array.sort(firstNumberToFront).join(''));
 }
 
 function addToArray() {
     let num = prompt("input number more or equal to zero");
-
-    if(!/^\d+$/.test(num)) return;
-
-    array.push(Number(num));
-    arrayOnPage.innerHTML = array;
+    if(!/^\d+$/.test(num) || (num.length > 1)&& num[0] == 0) return;
+    array.push(num);
+    arrayOnPage.innerHTML = array
 }
 function removeLastElemOfArray() {
     array.pop();
@@ -37,20 +33,7 @@ function clearArray() {
     array = [];
     arrayOnPage.innerHTML = array;
 }
-function bubbleSort(Array) {
 
-    for (let i = 0; i < Array.length-1; i++) {
-    	for (let j = 0; j < Array.length-1-i; j++) {
-    		if (firstNumberToFront(Array[j+1],Array[j])) {
-    			let temp = Array[j+1]; Array[j+1] = Array[j]; Array[j] = temp;
-    		}
-    	}
-    }
-    return Array;
-}
 function firstNumberToFront(num1, num2) {
-	let firstVariant = num1.toString() + num2.toString();
-	let secondVariant = num2.toString() + num1.toString();
-
-    return Number(firstVariant) >= Number(secondVariant);
+    return (num2.toString() + num1.toString()) - (num1.toString() + num2.toString());
 }
